6ca4882727fbb9f69a5ac54a26f06505dd168913
duncan-ross <44205757+duncan-ross@users.noreply.github.com>
2020-12-07 20:13:27 -0500

    Added final filtering functionality, it works now

---
+++ src/app/filter.js
@@ -0,0 +1,234 @@
+export function filter(criteria, filterKey, list)
+{
+    // inputs
+    var listSize = list.length;
+    var low = 0
+    var high = listSize - 1
+    
+    // output
+    var newList = []
+
+    // if the filterKey is a range (i.e. severity, visibility, or temperature)
+    if (Array.isArray(filterKey))
+    {
+        var min = filterKey[0]
+        var max = filterKey[1]
+
+        var firstOccurrence = first(list, low, high, min, listSize, criteria);
+        var lastOccurence  = last(list, low, high, max, listSize, criteria);
+    }
+    //  else filterKey NOT a range
+    else if (criteria == "date")
+    {
+        var firstOccurrence;
+        var date = new Date(filterKey);
+        date.setDate(date.getDate()+1);
+        console.log(date,date.getDate());
+        for (let i = 0; i < listSize; i++)
+        {
+          
+            var checker = new Date(list[i].Date.substring(0,8));
+            
+            if ((date.getDate()==checker.getDate())&&(date.getMonth()==checker.getMonth()))
+            {
+                firstOccurrence = i;
+                break;
+            }
+        }
+        var lastOccurence = firstOccurrence;
+        for (let j = firstOccurrence + 1; j < listSize; j++)
+        {
+            var checker = new Date(list[j].Date.substring(0,8))
+
+            if (!((date.getDate()==checker.getDate())&&(date.getMonth()==checker.getMonth())))
+            {
+                lastOccurence = j;
+                break;
+            }
+        }
+
+        console.log(firstOccurrence,lastOccurence)
+    }
+    else // time (day/night), state, or weather (alphabetical sort)
+    {
+        var firstOccurrence = first(list, low, high, filterKey, listSize, criteria)
+        var lastOccurence = last(list, low, high, filterKey, listSize, criteria)
+    }
+    
+
+    // Move the filtered data into a new list
+    for (let i = firstOccurrence; i < lastOccurence+1; i++)
+    {
+        // pushback the old in-range elements into a new list
+        newList.push(list[i]);
+    }
+
+    return newList;
+}
+
+// find the first occurence of a given val in an array
+function first(list, low, high, min, listSize, criteria)
+{
+    if (high >= low)
+    {
+        var mid = parseInt(low + (high - low) / 2);
+        if(mid==0){return 0;}
+        switch (criteria)
+        {
+            case "severity":
+                if (min == 1)
+                {
+                    return 0;
+                }
+                
+                var leftSplitter = list[mid-1].Sev
+                var rightSplitter = list[mid].Sev
+                break;
+            case "visibility":
+                var leftSplitter = list[mid-1].Vis
+                var rightSplitter = list[mid].Vis
+                break;
+            case "temperature":
+                var leftSplitter = list[mid-1].Temp
+                var rightSplitter = list[mid].Temp
+                break;
+            case "time":
+                if (min == "Day")
+                {
+                    return 0;
+                }
+                var leftSplitter = list[mid-1].Time
+                var rightSplitter = list[mid].Time
+                break;
+            case "state":
+                if (min < "CA")
+                {
+                    return;
+                }
+                else if (min == "CA")
+                {
+                    return 0;
+                } 
+            var leftSplitter = list[mid-1].State
+                var rightSplitter = list[mid].State
+                break;
+            case "weather":
+                var leftSplitter = list[mid-1].Weath
+                var rightSplitter = list[mid].Weath
+                break;
+    
+            case "date":
+                min = new Date(min)
+                min = min.toLocaleDateString('en-US');
+                var min = new Date(min)
+                var leftSplitter = new Date(list[mid-1].Date)
+                leftSplitter.setHours(0, 0, 0, 0)
+
+                var rightSplitter = new Date(list[mid].Date)
+                rightSplitter.setHours(0, 0, 0, 0)
+
+                if ((mid == 0 || min >= leftSplitter) && rightSplitter == min)
+                {
+                    return mid;
+                }
+                else if (min > rightSplitter)
+                {
+                    return first(list, mid + 1, high, min, listSize, criteria);
+                }
+                else
+                {
+                    return first(list, low, mid-1, min, listSize, criteria);
+                }
+                break;
+    
+            default:
+        }
+        if ((mid == 0 || min > leftSplitter) && rightSplitter == min)
+        {
+            return mid;
+        }
+        else if (min > rightSplitter)
+        {
+            return first(list, mid + 1, high, min, listSize, criteria);
+        }
+        else
+        {
+            return first(list, low, mid-1, min, listSize, criteria);
+        }
+    }
+    return -1;
+}
+
+// find the last occurence of a given val in an array
+function last(list, low, high, max, listSize, criteria)
+{
+    if (high >= low && high!=0)
+    {
+        var mid = parseInt(low + (high - low) / 2);
+        if(mid==0){return 0;}
+        switch (criteria)
+        {
+            case "severity":
+                var leftSplitter = list[mid-1].Sev
+                var rightSplitter = list[mid].Sev
+                break;
+            case "visibility":
+                var leftSplitter = list[mid-1].Vis
+                var rightSplitter = list[mid].Vis
+                break;
+            case "temperature":
+                var leftSplitter = list[mid-1].Temp
+                var rightSplitter = list[mid].Temp
+                break;
+            case "time":
+                var leftSplitter = list[mid-1].Time
+                var rightSplitter = list[mid].Time
+                break;
+            case "state":
+                if (max < "CA")
+                {
+                    return;
+                }
+                var leftSplitter = list[mid-1].State
+                var rightSplitter = list[mid].State
+                break;
+            case "weather":
+                var leftSplitter = list[mid-1].Weath
+                var rightSplitter = list[mid].Weath
+                break;
+    
+            case "date":
+                max = new Date(max)
+                max = max.toLocaleDateString('en-US');
+                var max = new Date(max)
+
+
+                var leftSplitter = new Date(list[mid-1].Date)
+                leftSplitter = leftSplitter.toLocaleDateString('en-US');
+                var leftSplitter = new Date(leftSplitter)
+
+                var rightSplitter = new Date(list[mid].Date)
+                rightSplitter = rightSplitter.toLocaleDateString('en-US');
+                var rightSplitter = new Date(rightSplitter)
+
+                break;
+    
+            default:
+        }
+
+        if ((mid == listSize - 1 || max < rightSplitter) && leftSplitter == max)
+        {
+            return mid;
+        }
+        else if (max < rightSplitter)
+        {
+            return last(list, low, mid - 1, max, listSize, criteria);
+        }
+        else
+        {
+            return last(list, mid + 1, high, max, listSize, criteria);
+        }
+    }
+    return -1;
+}
+
--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -3,7 +3,8 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 import { Options } from '@angular-slider/ngx-slider';
 import * as data from 'src/app/cut_down_header.json';
 import * as mergeSorter from 'src/app/mergesort.js';
-import * as heapSorter from 'src/app/heapsort.js'
+import * as heapSorter from 'src/app/heapsort.js';
+import * as dataFilter from 'src/app/filter.js';
 
 //stats interface to store all output stats
 interface stats {
@@ -16,6 +17,16 @@ interface stats {
   presMean: number,
   visMean: number,
   windDirection: String
+}
+interface queryControls{
+  sort:string,
+  date:string,
+  state:string,
+  time:string,
+  weather:string,
+  severity:any[],
+  visibility:any[],
+  temperature:any[]
 }
 //Main Driver Component for the Application//
 @Component({
@@ -46,7 +57,7 @@ export class MainFormComponent implements OnInit {
   sortList: any = ['MergeSort', 'HeapSort']
   stateList: any = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'WV'];
   timeList: any = ['Day', 'Night'];
-  weatherList: any = ['Clear', 'Haze', 'Drizzle', 'Light Rain', 'Rain', 'Heavy Rain', 'Scattered Clouds', 'Partly Cloudy', 'Mostly Cloudy', 'Overcast', 'Light Snow', 'Snow', 'Light Freezing Drizzle', 'Fog', 'Patches of Fog', 'Light Freezing Fog', 'Shallow Fog','Volcanic Ash','Widespread Dust'];
+  weatherList: any = ['Clear', 'Haze', 'Drizzle', 'Light Rain', 'Rain', 'Heavy Rain', 'Scattered Clouds', 'Partly Cloudy', 'Mostly Cloudy', 'Overcast', 'Light Snow', 'Snow', 'Light Freezing Drizzle', 'Fog', 'Patches of Fog', 'Light Freezing Fog', 'Shallow Fog', 'Volcanic Ash', 'Widespread Dust'];
 
   //configurations for sliders
   options1: Options = {
@@ -125,100 +136,118 @@ export class MainFormComponent implements OnInit {
    * Main driver code, calls all sorting functions, applies filters and generates report statistics.
    */
   generateSearch() {
-    const mS: Boolean = this.searchForm.controls.sortType.value == '1: MergeSort';
+    let query:queryControls ={
+      sort:this.searchForm.controls.sortType.value.substring(3).replace(/\s+/g, ''),
+      date:this.searchForm.controls.date.value,
+      state:this.searchForm.controls.state.value.substring(3).replace(/\s+/g, ''),
+      time:this.searchForm.controls.time.value.substring(3).replace(/\s+/g, ''),
+      weather:this.searchForm.controls.weather.value.substring(3).replace(/\s+/g, ''),
+      severity:this.searchForm.controls.severity.value,
+      visibility:this.searchForm.controls.visibility.value,
+      temperature:this.searchForm.controls.temperature.value,
+    }
     this.list = data;
     this.list = this.list.default;
     var start = Date.now();
 
+    console.log(query);
     //calls sort and filter on date
     if (this.searchForm.controls.date.value != '') {
       console.log(this.list);
-      if (mS) {
-        this.list = mergeSorter.mergeSort(this.list, 'date');
+      if (query.sort=='MergeSort') {
+        this.list=mergeSorter.mergeSort(this.list, 'date');
       }
-      else{
-        heapSorter.heapSort(this.list,'date');
+      else {
+        heapSorter.heapSort(this.list, 'date');
       }
       console.log(this.list);
-      //list = fede'sfunction(tlist);his.searchForm.controls.date.value,
+      this.list = dataFilter.filter('date', query.date, this.list);
+      console.log(this.list);
     }
 
     //calls sort and filter on state
     if (this.searchForm.controls.state.value != '') {
       console.log(this.list);
-      if (mS) {
-        this.list = mergeSorter.mergeSort(this.list, 'state');
+      if (query.sort=='MergeSort') {
+        this.list=mergeSorter.mergeSort(this.list, 'state');
       }
-      else{
-        heapSorter.heapSort(this.list,'state');
+      else {
+        heapSorter.heapSort(this.list, 'state');
       }
       console.log(this.list);
-      //list = fede'sfunction(this.searchForm.controls.state.value,list);
+      this.list = dataFilter.filter('state', query.state, this.list);
+      console.log(this.list);
     }
 
     //calls sort and filter on time
     if (this.searchForm.controls.time.value != '') {
       console.log(this.list);
-      if (mS) {
-        this.list = mergeSorter.mergeSort(this.list, 'time');
+      if (query.sort=='MergeSort') {
+        this.list=mergeSorter.mergeSort(this.list, 'time');
       }
-      else{
-        heapSorter.heapSort(this.list,'time');
+      else {
+        heapSorter.heapSort(this.list, 'time');
       }
       console.log(this.list);
-      //list = fede'sfunction(this.searchForm.controls.time.value,list);
+      this.list = dataFilter.filter('time', query.time, this.list);
+      console.log(this.list);
     }
 
     //calls sort and filter on weather
     if (this.searchForm.controls.weather.value != '') {
       console.log(this.list);
-      if (mS) {
-        this.list = mergeSorter.mergeSort(this.list, 'weather');
+      if (query.sort=='MergeSort') {
+        this.list=mergeSorter.mergeSort(this.list, 'weather');
       }
-      else{
-        heapSorter.heapSort(this.list,'weather');
+      else {
+        heapSorter.heapSort(this.list, 'weather');
       }
       console.log(this.list);
-      //list = fede'sfunction(this.searchForm.controls.weather.value,list);
+      this.list = dataFilter.filter('weather', query.weather, this.list);
+      console.log(this.list);
     }
 
     //calls sort and filter on severity
     if (this.searchForm.controls.severity.value[0] != 1 || this.searchForm.controls.severity.value[1] != 4) {
       console.log(this.list);
-      if (mS) {
-        this.list = mergeSorter.mergeSort(this.list, 'severity');
+      if (query.sort=='MergeSort') {
+        this.list=mergeSorter.mergeSort(this.list, 'severity');
       }
-      else{
-        heapSorter.heapSort(this.list,'severity');
+      else {
+        heapSorter.heapSort(this.list, 'severity');
       }
       console.log(this.list);
-      //list = fede'sfunction(this.searchForm.controls.severity.value,list);
+      this.list = dataFilter.filter('severity', query.severity, this.list);
+      console.log(this.list);
+
     }
 
     //calls sort and filter on visibility
     if (this.searchForm.controls.visibility.value[0] != 1 || this.searchForm.controls.visibility.value[1] != 10) {
       console.log(this.list);
-      if (mS) {
-        this.list = mergeSorter.mergeSort(this.list, 'visibility');
+      if (query.sort=='MergeSort') {
+        this.list=mergeSorter.mergeSort(this.list, 'visibility');
       }
-      else{
-        heapSorter.heapSort(this.list,'visibility');
+      else {
+        heapSorter.heapSort(this.list, 'visibility');
       }
       console.log(this.list);
-      //list = fede'sfunction(this.searchForm.controls.visibility.value,list);
+      this.list = dataFilter.filter('visibility', query.visibility, this.list);
+      console.log(this.list);
     }
 
     //calls sort and filter on temperature
     if (this.searchForm.controls.temperature.value[0] != -120 || this.searchForm.controls.temperature.value[1] != 120) {
       console.log(this.list);
-      if (mS) {
-        this.list = mergeSorter.mergeSort(this.list, 'temperature');
+      if (query.sort=='MergeSort') {
+        this.list=mergeSorter.mergeSort(this.list, 'temperature');
       }
-      else{
-        heapSorter.heapSort(this.list,'temperature');
+      else {
+        heapSorter.heapSort(this.list, 'temperature');
       }
       console.log(this.list);
-      //list = fede'sfunction(this.searchForm.controls.temperature.value,list);
+      this.list = dataFilter.filter('temperature', query.temperature, this.list);
+      console.log(this.list);
     }
     var end = Date.now();
     this.stats.time = end - start;
@@ -236,6 +265,7 @@ export class MainFormComponent implements OnInit {
 
 
     for (let i = 0; i < this.list.length; i++) {
+      if(this.list[i]){
       if (this.list[i].Side == 'L') {
         leftCounter++; //accumulator for left/right side crashes
       }
@@ -246,6 +276,7 @@ export class MainFormComponent implements OnInit {
       presTotal += +this.list[i].Pres;//accumulator for pressure
       visTotal += +this.list[i].Vis;//accumulator for visibility
     }
+  }
 
     //pushes all to our statistics object
     this.stats.leftSide = +(leftCounter / this.list.length * 100).toFixed(2);
@@ -259,7 +290,8 @@ export class MainFormComponent implements OnInit {
   }
   numberWithCommas(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+  }
+
 
 }
 
